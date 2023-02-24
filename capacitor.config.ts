0b38e896feebdbdae68a68e4e039ed0fb950bd2b
juandavid2502 <juandavidgm36@gmail.com>
2023-02-24 00:36:07 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'practica-3-angular',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
